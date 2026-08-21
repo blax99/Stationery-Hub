e6f28ef3c6b671138e2d3d7e472645b8f957dca5
Santoshi gurung <santoshigurung0276@gmail.com>
2026-08-21 11:25:05 +0545

    Add user registration API

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import RegisterView
 
 urlpatterns = [
-    #  URLs go here
+    path('register/', RegisterView.as_view(), name='register'),
 ]
