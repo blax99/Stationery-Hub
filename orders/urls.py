59ba509b1c9136963d3851eff2f000d99c0c8b02
jb2419226-dotcom <jb2419226@gmail.com>
2026-08-30 13:40:51 +0545

    Complete order checkout and confirmation

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/',views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
+    path(
+    'order-confirmation/<int:order_id>/',
+    views.order_confirmation,
+    name='order_confirmation'
+),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Order
 
 def checkout(request):
     return render(request, 'checkout.html')
 
+def checkout_confirmation(request, order_id):
+    order = get_object_or_404(
+        Order,
+        id=order_id,
+        user=request.user
+    )
 
+    return render(
+        request,
+        'order/confirmation.html',
+        {'order': order}
+    )
