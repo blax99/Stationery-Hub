697cf6b78431d5988d791f759a94f7b1c35f0527
Santoshi gurung <santoshigurung0276@gmail.com>
2026-08-30 13:03:26 +0545

    feat:Add password show/hide toggle on login and register

---
+++ ersACERDesktopStationery-HubvenvScriptsactivate.bat
@@ -0,0 +1,16 @@
+[33me2f251b[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mfeature/auth[m[33m, [m[1;31morigin/feature/auth[m[33m)[m feat: Add role-based permissions (IsAdmin, IsVendor, IsCustomer)
+[33md032dc0[m test: Add unit tests for registration and login
+[33mcd07ae3[m feat: Add phone and password validation to registration
+[33m1d28489[m feat: Add email verification after registration
+[33m244a1a9[m feat: Add profile page UI
+[33m3cabd0b[m feat: Add user profile view and edit API
+[33m3789827[m feat: Add register page UI and connect auth flow
+[33m1bfcac7[m feat: Add forgot password page and connect to login
+[33m65d4b36[m feat: Add login page UI
+[33mb90ce5f[m feat: Add password reset via email(testing in process)
+[33m9344331[m feat: Add JWT Login functionality
+[33me6f28ef[m Add user registration API
+[33meb8e9f8[m feat: Add custom User model with email login, phone and role fields
+[33ma71fdcd[m[33m ([m[1;31morigin/dev[m[33m, [m[1;32mdev[m[33m)[m docs: add README and contributing guide
+[33m54c7bc1[m Initial project structure
+[33m9bd72bf[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmain[m[33m)[m Initial commit
