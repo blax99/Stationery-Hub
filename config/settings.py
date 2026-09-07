5d70caea14101df89cb5efe325ef6231b7b07c9b
Krrish Chaudhary <chaudhariikrishna1@gmail.com>
2026-09-07 12:20:27 +0545

    chore: add khalti details

--- config/settings.py
+++ config/settings.py
@@ -142,7 +142,7 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-KHALTI_SECRET_KEY = "live_secret_key_xxx"  # or test_secret_key_xxx for sandbox
+KHALTI_SECRET_KEY = os.getenv("KHALTI_SECRET_KEY")  # or test_secret_key_xxx for sandbox
 # KHALTI_BASE_URL = "https://khalti.com/api/v2"        # prod
 KHALTI_BASE_URL = "https://dev.khalti.com/api/v2"  # sandbox
 KHALTI_WEBSITE_URL = "http://127.0.0.1:8000/"
